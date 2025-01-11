ed3732c135dffe0b6cd5d5d5d04a7ef6ddf3ad15
haseeburrehmanjs <kamranking786500@gmail.com>
2025-01-12 01:25:17 +0500

    modified work

--- Server/index.js
+++ Server/index.js
@@ -1,30 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import connectDB from "./src/db/index.js";
-import userRoutes from "./src/routes/users.routes.js";
+const express = require('express')
+const app = express()
+const port = 3000
 
-const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-// routes
-app.use("/api/v1", userRoutes);
-
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MONGO DB connection failed !!! ", err);
-  });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
